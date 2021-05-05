0b10c2ab2e9a974c9242585d12f4f07dd921b9c5
minariver <82023989+minariver@users.noreply.github.com>
2021-05-05 19:12:18 +0900

    エラーハンドリングの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :encrypted_password, format: with: /\A[a-z0-9]+\z/i 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   with_options presence: true do
     validates :nickname
-    validates :famiy_name, format: with: /\A[ぁ-んァ-ン一-龥]/
-    validates :first_name, format: with: /\A[ぁ-んァ-ン一-龥]/
-    validates :famiy_name_kana, format: with: /\A[ァ-ヶー－]+\z/
-    validates :first_name_kana, format: with: /\A[ァ-ヶー－]+\z/
+    validates :famiy_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :famiy_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
   end
 
