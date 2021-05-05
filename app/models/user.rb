89b6cb0f7514019c34e4f76b96d90cd15b54f3d7
minariver <82023989+minariver@users.noreply.github.com>
2021-05-05 15:09:20 +0900

    Userモデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :encrypted_password, format: with: /\A[a-z0-9]+\z/i 
+  with_options presence: true do
+    validates :nickname
+    validates :famiy_name, format: with: /\A[ぁ-んァ-ン一-龥]/
+    validates :first_name, format: with: /\A[ぁ-んァ-ン一-龥]/
+    validates :famiy_name_kana, format: with: /\A[ァ-ヶー－]+\z/
+    validates :first_name_kana, format: with: /\A[ァ-ヶー－]+\z/
+    validates :birthday
+  end
+
 end
