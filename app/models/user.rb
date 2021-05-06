61b74f6ffe9af45fcaf7c4ab07dd15019ea74aac
minariver <82023989+minariver@users.noreply.github.com>
2021-05-06 18:10:06 +0900

    ユーザー管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,14 @@ class User < ApplicationRecord
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   with_options presence: true do
     validates :nickname
-    validates :famiy_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :famiy_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]/ }do
+      validates :famiy_name
+      validates :first_name
+    end
+    with_options format:{ with: /\A[ァ-ヶー－]+\z/ }do
+      validates :famiy_name_kana
+      validates :first_name_kana
+    end
     validates :birthday
   end
-
 end
