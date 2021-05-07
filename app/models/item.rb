c3515b94b5750bc9d796a3da151d671595414756
minariver <82023989+minariver@users.noreply.github.com>
2021-05-07 19:46:55 +0900

    プルダウン配送料の負担・発送までの日数の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
+  belongs_to :Category
   belongs_to :status
+  belongs_to :Postage
+  belongs_to :prefecture
+  belongs_to :shipment
 end
