a624d640e93a1b5a19b0ec5d3918089e2d4ee901
minariver <82023989+minariver@users.noreply.github.com>
2021-05-07 15:36:37 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,13 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :product_detail, :price, :category_id, :status_id, :postage_id, :prefecture_id, :shipment_id, :image).merge(user_id: current_user.id)
+  end
+
 end
