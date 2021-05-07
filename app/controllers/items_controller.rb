99eaf826be3caaf61ceef884bd071d8cd6510d67
minariver <82023989+minariver@users.noreply.github.com>
2021-05-07 18:24:58 +0900

    カテゴリー都道府県の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,15 +6,27 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @prefectures = Prefecture.order("created_at DESC")
+    @prefecture = Prefecture.new
   end
 
   def create
     Item.create(item_params)
+    @Prefecture = Prefecture.new(prefecture_params)
+    if @prefecture.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :product_detail, :price, :category_id, :status_id, :postage_id, :prefecture_id, :shipment_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :product_detail, :price, :category_id, :status_id, :postage_id, :prefecture_id, :shipment_id, :image)
+  end
+
+  def prefecture_params
+    params.require(:prefecture).permit(:prefecture_id)
   end
 
 end
