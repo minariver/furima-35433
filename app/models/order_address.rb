30b7dc9718f90ef6aec0118b5146e4c497f8c421
minariver <82023989+minariver@users.noreply.github.com>
2021-05-17 14:33:25 +0900

    商品出品機能の修正5

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :order_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token
   with_options presence: true do
     validates :token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
